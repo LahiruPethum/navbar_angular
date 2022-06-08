49fe3468eaf8c62a8618bcf3b2c4346e90b5152a
LahiruPethum <pethum013@gmail.com>
2022-06-08 15:07:58 +0530

    dark theme added

---
+++ src/app/shared/nav/nav.component.ts
@@ -0,0 +1,40 @@
+import { DOCUMENT } from '@angular/common';
+
+import { Component, OnInit, Inject, Renderer2 } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
+})
+export class NavComponent implements OnInit {
+
+  theme: Theme = 'light-theme';
+url="../../../assets/images/moon.png";
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private renderer: Renderer2
+  ) {}
+
+  ngOnInit() {
+    this.initializeTheme();
+  }
+
+  switchTheme() {
+    this.document.body.classList.replace(
+      this.theme,
+      this.theme === 'light-theme'
+        ? (this.theme = 'dark-theme')
+        : (this.theme = 'light-theme')
+    );
+    this.url.replace(
+      this.url,
+      this.theme==='light-theme' ?(this.url="../../assets/images/moon.png"):(this.url="../../assets/images/sun.png")
+    );
+  }
+
+  initializeTheme = (): void =>
+    this.renderer.addClass(this.document.body, this.theme);
+}
+
+export type Theme = 'light-theme' | 'dark-theme';
